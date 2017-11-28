737c0a677fa445ebdfdd0a17b60f3e56bee8184f
allen <501671402@qq.com>
2017-11-28 21:53:24 +0800

    log4j

--- shout-user/shout-user-facade-impl/src/main/java/com/allen/shout/user/facade/impl/UserBaseFacade.java
+++ shout-user/shout-user-facade-impl/src/main/java/com/allen/shout/user/facade/impl/UserBaseFacade.java
@@ -5,16 +5,19 @@ import com.allen.shout.user.application.IUserBaseApplication;
 import com.allen.shout.user.core.domain.UserBase;
 import com.allen.shout.user.facade.IUserBaseFacade;
 import com.allen.shout.user.facade.dto.UserBaseDTO;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Service(version = "1.0.0")
 public class UserBaseFacade implements IUserBaseFacade {
 
+    public static Logger logger = Logger.getLogger(UserBaseFacade.class);
     @Autowired
     private IUserBaseApplication userBaseApplication;
 
     @Override
     public UserBaseDTO test() {
+        logger.error("method test begin...");
         UserBaseDTO userBaseDTO = new UserBaseDTO();
         UserBase user = userBaseApplication.test();
         if(user != null){
