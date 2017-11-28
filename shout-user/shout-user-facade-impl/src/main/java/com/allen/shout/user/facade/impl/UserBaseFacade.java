4629a572a237d7dca2193176213f3ee6886e71da
yuanchen.li <yuanchen.li.hn@gmail.com>
2017-11-28 17:28:16 +0800

    dubbo test

--- shout-user/shout-user-facade-impl/src/main/java/com/allen/shout/user/facade/impl/UserBaseFacade.java
+++ shout-user/shout-user-facade-impl/src/main/java/com/allen/shout/user/facade/impl/UserBaseFacade.java
@@ -1,7 +1,6 @@
 package com.allen.shout.user.facade.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
-import com.alibaba.fastjson.JSON;
 import com.allen.shout.user.application.IUserBaseApplication;
 import com.allen.shout.user.core.domain.UserBase;
 import com.allen.shout.user.facade.IUserBaseFacade;
