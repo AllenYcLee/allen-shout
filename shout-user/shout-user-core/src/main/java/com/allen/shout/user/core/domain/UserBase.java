a2025e46031db42a020e458c413fc6417d146809
yuanchen.li <501671402@qq.com>
2017-11-28 10:08:43 +0800

    userMapper

--- shout-user/shout-user-core/src/main/java/com/allen/shout/user/core/domain/UserBase.java
+++ shout-user/shout-user-core/src/main/java/com/allen/shout/user/core/domain/UserBase.java
@@ -6,7 +6,6 @@ import com.allen.shout.user.core.repository.IUserBaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.xml.ws.soap.Addressing;
 
 @Component
 public class UserBase extends Entity {
