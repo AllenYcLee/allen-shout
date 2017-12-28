98da1410ab1854bcc71c1ba5bba5146d81d6855b
yuanchen.li <yuanchen.li.hn@gmail.com>
2017-12-28 11:12:43 +0800

    logback configuration

--- shout-user/shout-user-facade-impl/src/main/java/com/allen/shout/user/facade/impl/UserBaseFacade.java
+++ shout-user/shout-user-facade-impl/src/main/java/com/allen/shout/user/facade/impl/UserBaseFacade.java
@@ -5,20 +5,18 @@ import com.allen.shout.user.application.IUserBaseApplication;
 import com.allen.shout.user.core.domain.UserBase;
 import com.allen.shout.user.facade.IUserBaseFacade;
 import com.allen.shout.user.facade.dto.UserBaseDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-
+@Slf4j
 @Service(version = "1.0.0")
 public class UserBaseFacade implements IUserBaseFacade {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserBaseFacade.class);
     @Autowired
     private IUserBaseApplication userBaseApplication;
 
     @Override
     public UserBaseDTO test() {
-        logger.error("method test begin...");
+        log.error("method test begin...");
         UserBaseDTO userBaseDTO = new UserBaseDTO();
         UserBase user = userBaseApplication.test();
         if(user != null){
